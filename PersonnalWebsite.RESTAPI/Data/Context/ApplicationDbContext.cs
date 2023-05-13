22f064c03e5392656539baa8ed394c7af47c3b75
T-Hamel94 <tristan_hamel@hotmail.com>
2023-05-13 12:48:59 -0400

    Base project for the REST API

---
+++ PersonnalWebsite.RESTAPI/Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace PersonnalWebsite.RESTAPI.Data.Context
+{
+    public class ApplicationDbContext
+    {
+    }
+}
---
+++ Srv_PersonnalWebsite/Entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srv_PersonnalWebsite.Entity
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+    }
+}
